15a714b0ebc5a792c536659c53465a00b16609bf
LucasSmith1212 <89419649+LucasSmith1212@users.noreply.github.com>
2021-08-25 14:20:11 -0700

    Add files via upload

---
+++ CS 155 - Homework 1.cs
@@ -0,0 +1,30 @@
+/// Chapter No. 1 Exercise No. 2
+/// File Name: Homework1.cs
+/// @author: Lucas Smith
+/// Date:  August 25, 2021
+///
+/// Problem Statement: Draws a 3-D box using Console commands.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print a 3-D box using multiple Console.WriteLine commands
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+    class Homework1 {
+        static void Main(string[] args) {
+            Console.WriteLine("   .+------+");
+            Console.WriteLine(" .' |    .'|");
+            Console.WriteLine("+---+--+'  |");
+            Console.WriteLine("|   |  |   |");
+            Console.WriteLine("|  ,+--+---+");
+            Console.WriteLine("|.'    | .' ");
+            Console.WriteLine("+------+'   ");
+        }
+    }
+}
